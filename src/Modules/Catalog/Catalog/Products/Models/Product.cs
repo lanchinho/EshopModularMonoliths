8aee715cafbb9db6f9d691bc16f6267fddab7432
Guilherme da Costa de Albuquerque <guicalbuquerque@gmail.com>
2026-01-31 16:01:46 -0300

    refactor.

--- src/Modules/Catalog/Catalog/Products/Models/Product.cs
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -1,49 +1,49 @@
 ﻿namespace Catalog.Products.Models
 {
-	public class Product : Aggregate<Guid>
-	{
-		public string Name { get; private set; } = default!;
-		public List<string> Category { get; private set; } = [];
-		public string Description { get; private set; } = default!;
-		public string ImageFile { get; private set; } = default!;
-		public decimal Price { get; private set; }
+    public class Product : Aggregate<Guid>
+    {
+        public string Name { get; private set; } = default!;
+        public List<string> Category { get; private set; } = [];
+        public string Description { get; private set; } = default!;
+        public string ImageFile { get; private set; } = default!;
+        public decimal Price { get; private set; }
 
-		public static Product Create(Guid id, string name, List<string> category, string description, string imageFile, decimal price)
-		{
-			ArgumentException.ThrowIfNullOrEmpty(name);
-			ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+        public static Product Create(Guid id, string name, List<string> category, string description, string imageFile,
+            decimal price)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
 
-			var product = new Product
-			{
-				Id = id,
-				Name = name,
-				Category = category,
-				Description = description,
-				ImageFile = imageFile,
-				Price = price
-			};
+            var product = new Product
+            {
+                Id = id,
+                Name = name,
+                Category = category,
+                Description = description,
+                ImageFile = imageFile,
+                Price = price
+            };
 
-			product.AddDomainEvent(new ProductCreatedEvent(product));
+            product.AddDomainEvent(new ProductCreatedEvent(product));
 
-			return product;
-		}
+            return product;
+        }
 
-		public void Update(string name, List<string> category, string description, string imageFile, decimal price)
-		{
-			ArgumentException.ThrowIfNullOrEmpty(name);
-			ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+        public void Update(string name, List<string> category, string description, string imageFile, decimal price)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
 
-			Name = name;
-			Category = category;
-			Description = description;
-			ImageFile = imageFile;
-			Price = price;
-			
-			if(Price != price)
-			{
-				Price = price;
-				AddDomainEvent(new ProductPriceChangedEvent(this));
-			}
-		}
-	}
+            Name = name;
+            Category = category;
+            Description = description;
+            ImageFile = imageFile;
+            
+            if (Price == price)
+                return;
+            
+            Price = price;
+            AddDomainEvent(new ProductPriceChangedEvent(this));
+        }
+    }
 }

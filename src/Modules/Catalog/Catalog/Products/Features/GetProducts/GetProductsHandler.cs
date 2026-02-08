034c1a8a83e7925039dfbd53c5dcec1d43dac88e
Guilherme da Costa de Albuquerque <guicalbuquerque@gmail.com>
2026-02-08 17:07:51 -0300

    add pagination.

--- src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsHandler.cs
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsHandler.cs
@@ -1,21 +1,32 @@
 ﻿namespace Catalog.Products.Features.GetProducts
 {
-	public record GetProductsQuery()
+	public record GetProductsQuery(PaginationRequest PaginationRequest)
 		: IQuery<GetProductsResult>;
 
-	public record GetProductsResult(IEnumerable<ProductDto> Products);
+	public record GetProductsResult(PaginatedResult<ProductDto> Products);
 
 	internal class GetProductsHandler(CatalogDbContext dbContext)
 		: IQueryHandler<GetProductsQuery, GetProductsResult>
 	{
 		public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
 		{
+			var pageIndex = query.PaginationRequest.PageIndex;
+			var pageSize = query.PaginationRequest.PageSize;
+			var totalCount = await dbContext.Products.AsNoTracking().LongCountAsync(cancellationToken);
+
 			var products = await dbContext.Products
 				.AsNoTracking()
 				.OrderBy(p => p.Name)
+				.Skip(pageSize * pageIndex)
+				.Take(pageSize)
 				.ToListAsync(cancellationToken);
 			
-			return new GetProductsResult(products.Adapt<List<ProductDto>>());
+			return new GetProductsResult(
+				new PaginatedResult<ProductDto>(
+					pageIndex,
+					pageSize,
+					totalCount,
+					products.Adapt<List<ProductDto>>()));
 		}
 	}
 }
---
+++ src/Shared/Shared/Pagination/PaginationRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace Shared.Pagination
+{
+	public record PaginationRequest(int PageIndex = 0, int PageSize = 10);
+}
